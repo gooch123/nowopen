ac4288575bcad2b5c33d824a38ec58ee3b06a574
gooch <gooch123@naver.com>
2024-03-27 21:52:06 +0900

    Initial commit

---
+++ src/main/java/yu/nowopen/NowopenApplication.java
@@ -0,0 +1,13 @@
+package yu.nowopen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowopenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NowopenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yu/nowopen/NowopenApplicationTests.java
@@ -0,0 +1,13 @@
+package yu.nowopen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NowopenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
